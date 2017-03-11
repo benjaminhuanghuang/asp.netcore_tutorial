8f1bf638a48089018b251c2aceb2cbaae17c6bf1
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-03-11 07:46:26 -0800

    update part 4

--- part4/modern/src/main.ts
+++ part4/modern/src/main.ts
@@ -21,13 +21,15 @@ export async function configure(aurelia: Aurelia) {
   // Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
   // aurelia.use.plugin('aurelia-html-import-template-loader')
 
-  
-  // aurelia.setRoot('app');
+  // Original
+  //await aurelia.start();
+  //aurelia.setRoot('app');
+  //--Change to
   const rootElement = document.body;
   rootElement.setAttribute("aurelia-app","");
   await aurelia.start();
-  aurelia.setRoot('gretter', rootElement);
-
+  aurelia.setRoot('greeter', rootElement);
+  //--end
   // if you would like your website to work offline (Service Worker), 
   // install and enable the @easy-webpack/config-offline package in webpack.config.js and uncomment the following code:
   /*
