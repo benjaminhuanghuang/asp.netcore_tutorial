96e2e4645e55d077f4b0d42ca94bc9c0f7956abc
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-03-11 09:51:45 -0800

    add sub view

--- part5/modern/src/task-manager/index.ts
+++ part5/modern/src/task-manager/index.ts
@@ -1,3 +1,25 @@
-export class Index{
+import { Router, RouterConfiguration } from "aurelia-router";
 
+export class Index {
+    router: Router;
+
+    configureRouter(config: RouterConfiguration, router: Router) {
+        config.title = "Aurelia Contact Manager";
+        config.map([
+            {
+                route: "",
+                moduleId: "contact-manaer/no-selection",
+                nav: true,
+                name: "contactmanager",
+                title: "Contact Manager"
+            },
+            {
+                route: "contacts/:id",
+                moduleId: "contact-manager/contact-detail",
+                nave: true,
+                name: "contacts"
+            }
+        ]);
+        this.router = router;
+    }
 }
